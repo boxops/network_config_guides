036f69676871d902ad85c2c5a8301f7abe02cb09
Bal <olahbalazs@outlook.com>
2022-07-25 17:25:33 +0100

    Update junos_multi_backup_config.py

--- Junos_OS/automation/junos_multi_backup_config.py
+++ Junos_OS/automation/junos_multi_backup_config.py
@@ -2,63 +2,21 @@
 # pip3 install junos-eznc lxml
 
 # Usage
-# python3 junos_get_config.py
+# python3 junos_multi_backup_config.py
 
 from jnpr.junos import Device
 from lxml import etree
 import os
 
 hosts = [
-    "185.96.18.71", #HELA
-    "185.96.18.45", #BENETT
-    "185.96.18.49", #VALET
-    "185.96.18.35", #TDA
-    "185.96.18.37", #MARGOT
-    "185.96.18.47", #LUMON
-    "185.96.18.51", #LHH
-    "185.96.18.89", #NAILS
-    "185.96.18.65", #HURFORD
-    "185.96.18.97", #TEACH
-    "185.96.18.105", #CNET
-    "185.96.18.107", #CNET
-    "185.96.18.109", #CNET
-    "185.96.18.115", #CNET
-    "185.96.18.117", #CNET
-    "185.96.18.119", #FERRA
-    "185.96.18.211", #VAP4
-    "185.96.18.217", #VAP2
-    "185.96.18.241", #VIROCOM
-    "185.96.18.169", #RON
-    "185.96.18.59", #RON
-    "185.96.18.75", #RON
-    "185.96.18.61", #JACKSON
-    "185.96.18.15", #HYTERA
-    "185.96.18.31", #TBUK-HO-2
-    "185.96.18.29", #TBUK-HO-1
-    "185.96.18.55", #PEPKOR-HO-2
-    "185.96.16.218", #ERCO
-    "185.96.18.93", #TEACH
-    "185.96.18.25", #ICLR
-    "185.96.18.39", #HURFORD
-    "185.96.18.87", #PEPKOR-HO-1
-    "185.96.18.63", #APOLLO
-    "185.96.18.95", #TEACH
-    "185.96.18.99", #TEACH
-    "91.109.47.22", #CNET
-    "91.109.47.66", #CNET
-    "91.109.47.86", #CNET
-    "185.96.18.111", #CNET
-    "91.109.47.14", #CNET
-    "185.96.18.21", #FERRA
-    "91.109.47.2", #CNET
-    "91.109.47.122", #CNET
-    "185.96.18.123", #RED
-    "91.109.42.94", #MEGA
-    "91.109.42.92" #MEGA
+    "11.11.11.11",
+    "11.11.11.12",
+    "11.11.11.13",
+    "11.11.11.14"
 ]
 
-user = "netadmin"
-password = "H3st1a3n!"
+user = "admin"
+password = "Super23Secure34Password!"
 port = 22
 
 online_hosts = []
