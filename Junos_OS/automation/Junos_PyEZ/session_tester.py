e78ae84df2b7a1c0fa84193895e49885008d76c5
Bal <olahbalazs@outlook.com>
2022-07-30 22:44:39 +0100

    Update session_tester.py

--- Junos_OS/automation/Junos_PyEZ/session_tester.py
+++ Junos_OS/automation/Junos_PyEZ/session_tester.py
@@ -1,14 +1,16 @@
-# Purpose
-# The following sample code prints the value of the connected property after connecting to a device running Junos OS and again after closing the session.
+"""
+Purpose
+The following sample code prints the value of the connected property after connecting to a device running Junos OS and again after closing the session.
 
-# Requirements
-# python -m pip install junos-eznc lxml
+Requirements
+python -m pip install junos-eznc lxml
 
-# Usage
-# python3 session_tester.py
+Usage
+python session_tester.py
 
-# Reference
-# https://www.juniper.net/documentation/us/en/software/junos-pyez/junos-pyez-developer/topics/topic-map/junos-pyez-connection-methods.html
+Reference
+https://www.juniper.net/documentation/us/en/software/junos-pyez/junos-pyez-developer/topics/topic-map/junos-pyez-connection-methods.html
+"""
 
 from jnpr.junos import Device
 
