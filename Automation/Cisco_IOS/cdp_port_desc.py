1076672869d7b8165d8a29d9adce8e7a31fe6e8c
boxops <olahbalazs@outlook.com>
2022-08-25 23:12:48 +0100

    CDP script correction

--- Automation/Cisco_IOS/cdp_port_desc.py
+++ Automation/Cisco_IOS/cdp_port_desc.py
@@ -3,10 +3,8 @@ from scrapli.driver.core import IOSXEDriver
 from rich import print as pr
 
 devices = [
-    {"host": "192.168.100.1", "auth_username": "cisco", "auth_password": "cisco", "ssh_config_file": True, "auth_strict_key": False},
-    {"host": "192.168.100.2", "auth_username": "cisco", "auth_password": "cisco", "ssh_config_file": True, "auth_strict_key": False},
-    {"host": "192.168.100.3", "auth_username": "cisco", "auth_password": "cisco", "ssh_config_file": True, "auth_strict_key": False},
-    {"host": "192.168.100.4", "auth_username": "cisco", "auth_password": "cisco", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "192.168.111.1", "auth_username": "admin", "auth_password": "Cisco123", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "192.168.111.100", "auth_username": "admin", "auth_password": "Cisco123", "ssh_config_file": True, "auth_strict_key": False}
 ]
 
 for device in devices:
@@ -20,7 +18,6 @@ for device in devices:
             remote_port = cdp_index[item]['port_id']
             local_port = cdp_index[item]['local_interface']
             config_commands = [f"interface {local_port}", f"description Connected to {remote_device}'s {remote_port}"]
-            results = connection.send_config_set(config_commands)
+            results = connection.send_configs(config_commands)
             pr(results.result)
         pr("*** Config Sent ***\n")
-
