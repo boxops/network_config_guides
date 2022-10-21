4fb09576f6257e185e575677b949bd04038c395f
Bal <olahbalazs@outlook.com>
2022-10-21 12:46:03 +0100

    Add files via upload

---
+++ General_Guides/Git/generate_ssh_keys.sh
@@ -0,0 +1,12 @@
+# Silently generate SSH keys
+
+# Reference
+# https://stackoverflow.com/questions/43235179/how-to-execute-ssh-keygen-without-prompt
+
+echo SSH key filename:
+
+read filename
+
+cd ~/.ssh
+
+ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/$filename <<<y >/dev/null 2>&1
