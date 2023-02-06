a4fbb4110cf770cc85bb8e5785b4a43028e0cfc4
Bal <olahbalazs@outlook.com>
2023-02-06 12:06:03 +0000

    Create dhcp-build.sh

---
+++ General_Guides/Linux/Kea_DHCP/kea_dhcp_server_project/dhcp-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Purpose: Deploy Kea DHCP or Stork on localhost
+
+# Prompt: Deploy Kea DHCP server or Stork server? ([D]/S)
+# Action: Show environment variables for selected
+
+# Prompt: Proceed with the deployment? (Y/[N])
+# Action: Deploy or abort
