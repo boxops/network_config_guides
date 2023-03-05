b8b41c46e9697924b6190f04c6587b985978a679
Bal <olahbalazs@outlook.com>
2023-03-05 22:31:34 +0000

    Update and rename dhcp-build.sh to kea-build.sh

--- General_Guides/Linux/Kea_DHCP/kea_dhcp_server_project/kea-build.sh
+++ General_Guides/Linux/Kea_DHCP/kea_dhcp_server_project/kea-build.sh
@@ -2,7 +2,13 @@
 # Purpose: Deploy Kea DHCP or Stork on localhost
 
 # Prompt: Deploy Kea DHCP server or Stork server? (D/s)
+read -p "Deploy Kea DHCP server or Stork server? (D/s) " -n 1 -r
+
 # Action: Show environment variables for selected
+echo "Environment variables for selected:"
 
 # Prompt: Proceed with the deployment? (y/N)
+read -p "Proceed with the deployment? (y/N) " -n 1 -r
+
 # Action: Deploy or abort
+echo "Deploy or abort"
